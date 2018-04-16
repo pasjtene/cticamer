fc083320f36cbc88deacb912210cfcb2aebca9e6
DimitriKENGNE <m.dimitri.kengne@gmail.com>
2018-04-16 13:30:21 +0100

    Tentative de verification d"un email avec une regex

--- src/Web/MainBundle/Resources/public/js/cti/nous-contacter.js
+++ src/Web/MainBundle/Resources/public/js/cti/nous-contacter.js
@@ -3,3 +3,24 @@ function majusCule(formField) {
     x.value = x.value.toUpperCase();
 }
 
+function surligner (champ, erreur) {
+    if (erreur){
+        champ.style.borderBottomColor = "red";
+    }
+    else {
+        champ.style.borderBottomColor = "";
+    }
+}
+
+function EmailVerification(formField) {
+    var x = document.getElementById(formField.id),
+        re = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9\._-]{2,}\.[a-z]{2,4}$/;
+    if (!re.match(x.value)) {
+        surligner(x, true);
+        return false;
+    }
+    else {
+        surligner(x, false);
+        return true;
+    }
+}
